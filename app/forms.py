e61558cd3e7392b6256797732f35f41d15621198
Hyelda <hndzarma@gmail.com>
2022-07-30 23:12:19 +0100

    add first remote

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,12 @@
 from flask_wtf import FlaskForm
-from flask_wtf.html5 import EmailField, URLField
-from wtforms import TextField, PasswordField, SubmitField, BooleanField
+from wtforms import PasswordField, SubmitField, BooleanField, StringField
 from wtforms.validators import EqualTo, InputRequired, Length
 
 
 
 class SignUpForm(FlaskForm):
-    email = EmailField('')
-    username = TextField('', validators=[InputRequired('field is required'), Length(
+    email = StringField('')
+    username = StringField('', validators=[InputRequired('field is required'), Length(
         max=15, message='username cannot be more than 15 characters long')])
     password = PasswordField('', validators=[InputRequired('field is required'), Length(
         min=8, message='password must be at least 8 characters')])
@@ -17,14 +16,14 @@ class SignUpForm(FlaskForm):
 
 
 class SignInForm(FlaskForm):
-    email = EmailField('', validators=[InputRequired('field is required')])
+    email = StringField('', validators=[InputRequired('field is required')])
     password = PasswordField('', validators=[InputRequired('field is required')] )
     remember_me = BooleanField('remember me?')
     login = SubmitField('Sign In')
 
     
 class UrlAddForm(FlaskForm):
-    name = TextField('', validators=[ InputRequired('field is required'), 
+    name = StringField('', validators=[ InputRequired('field is required'), 
                     Length(max = 30,message='name cannot be more than 30 characters long')])
-    url = URLField(validators=[InputRequired('field is required')])
+    url = StringField(validators=[InputRequired('field is required')])
     add_url = SubmitField('Add')
